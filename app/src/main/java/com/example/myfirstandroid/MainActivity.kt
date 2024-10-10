147bbf5038be7a7b00728711ede0d751e1856798
gus ryan <rian.putra2003@gmail.com>
2024-10-10 22:34:34 +0800

    add the fragment component view

--- app/src/main/java/com/example/myfirstandroid/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroid/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myfirstandroid
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -26,6 +27,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val homeFragment = BlankFragment()
+        val fragment = fragmentManager.findFragmentByTag(BlankFragment::class.java.simpleName)
+        if (fragment !is BlankFragment) {
+            Log.d("MyFlexibleFragment", "Fragment Name :" + BlankFragment::class.java.simpleName)
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, homeFragment, BlankFragment::class.java.simpleName)
+                .commit()
+        }
+
         edtWidth = findViewById(R.id.edt_width)
         edtHeight = findViewById(R.id.edt_height)
         edtLength = findViewById(R.id.edt_length)
